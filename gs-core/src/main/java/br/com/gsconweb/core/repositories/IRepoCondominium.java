6df7cdb0cae48f82bea8b6f3537a8ef11d3821f2
rafasystec <rafasystec@yahoo.com.br>
2019-03-27 14:29:30 -0300

    Excluding interface repository.

--- gs-core/src/main/java/br/com/gsconweb/core/repositories/IRepoCondominium.java
+++
@@ -1,8 +0,0 @@
-package br.com.gsconweb.core.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import br.com.gsconweb.core.model.Condominium;
-
-public interface IRepoCondominium extends CrudRepository<Condominium, Long>{
-
-}
